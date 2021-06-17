6b816f20b58f4a6cfac02a258002f9d6c64a90f0
kgwny <kigawanaoya@gmail.com>
2021-06-17 20:39:52 +0900

    add snippet

--- basic/array7.js
+++ basic/array7.js
@@ -1,12 +1,15 @@
+// 配列71
 //===========================================
 var csv71 = "4,5,6,13,14,15";
 var array71 = [];
 var rows71 = csv71.split(",");
-var cnt7 = 3;
+// 要素を3つずつに分割して配列へ設定する
+var separeteNum = 3;
 for (var i = 0; rows71.length > 0; i++) {
-    array71[i] = rows71.slice(0, cnt7);
-    rows71.splice(0, cnt7);
+    array71[i] = rows71.slice(0, separeteNum);
+    rows71.splice(0, separeteNum);
 }
+// 配列72
 //===========================================
 var csv72 = "1,2,3\n4,5,6\n7,8,9\n10,11,12";
 var array72 = [];
@@ -15,11 +18,13 @@ for (var i = 0; i < rows72.length; i++) {
     array72[i] = rows72[i].split(",");
 }
 //===========================================
-// 含まれることを確認する
+// 配列71の各要素が配列72に含まれることを確認する
 for (var i = 0; i < array71.length; i++) {
     var tmp1 = JSON.stringify(array71[i]);
+    console.log("tmp1 = " + tmp1);
     for (var j = 0; j < array72.length; j++) {
         var tmp2 = JSON.stringify(array72[j]);
+        console.log("tmp2 = " + tmp2);
         console.log("tmp1 === tmp2 (" + tmp1 + ", " + tmp2 + ")");
         if (tmp1 === tmp2) {
             console.log("true");
--- basic/array7.ts
+++ basic/array7.ts
@@ -1,14 +1,17 @@
+// 配列71
 //===========================================
 const csv71 = "4,5,6,13,14,15";
 
 let array71 = [];
 let rows71 = csv71.split(",");
 
-const cnt7 = 3;
+// 要素を3つずつに分割して配列へ設定する
+const separeteNum = 3;
 for (let i = 0; rows71.length > 0; i++) {
-    array71[i] = rows71.slice(0, cnt7);
-    rows71.splice(0, cnt7);
+    array71[i] = rows71.slice(0, separeteNum);
+    rows71.splice(0, separeteNum);
 }
+// 配列72
 //===========================================
 const csv72 = "1,2,3\n4,5,6\n7,8,9\n10,11,12";
 
@@ -19,12 +22,15 @@ for (let i = 0; i < rows72.length; i++) {
     array72[i] = rows72[i].split(",");
 }
 //===========================================
-// 含まれることを確認する
+// 配列71の各要素が配列72に含まれることを確認する
 for (let i = 0; i < array71.length; i++) {
     let tmp1 = JSON.stringify(array71[i]);
+    console.log("tmp1 = " + tmp1);
 
     for (let j = 0; j < array72.length; j++) {
         let tmp2 = JSON.stringify(array72[j]);
+        console.log("tmp2 = " + tmp2);
+
         console.log("tmp1 === tmp2 (" + tmp1 + ", " + tmp2 + ")");
         if (tmp1 === tmp2) {
             console.log("true");

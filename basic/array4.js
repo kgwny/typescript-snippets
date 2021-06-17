c88aad75da6387419acad8fc2c673f987a8f54e0
kgwny <kigawanaoya@gmail.com>
2021-06-17 14:33:48 +0900

    add snippet

--- basic/array4.js
+++ basic/array4.js
@@ -3,12 +3,13 @@ console.log(csv4);
 // 1,2,3,4,,,7,8,9,10,11,12,
 var array4 = [];
 var rows4 = csv4.split(",");
-var splitNum = 5;
+// 配列から抽出する要素数
+var elementCount = 5;
 for (var i = 0; rows4.length > 0; i++) {
     // 配列から指定した要素を取得
-    array4[i] = rows4.slice(0, splitNum);
+    array4[i] = rows4.slice(0, elementCount);
     // 配列から指定した要素を削除
-    rows4.splice(0, splitNum);
+    rows4.splice(0, elementCount);
 }
 console.log(array4);
 // 1,2,3,4,,,7,8,9,10,11,12,
--- basic/array4.ts
+++ basic/array4.ts
@@ -6,13 +6,14 @@ console.log(csv4);
 let array4 = [];
 let rows4 = csv4.split(",");
 
-const splitNum = 5;
+// 配列から抽出する要素数
+const elementCount = 5;
 
 for (let i = 0; rows4.length > 0; i++) {
     // 配列から指定した要素を取得
-    array4[i] = rows4.slice(0, splitNum);
+    array4[i] = rows4.slice(0, elementCount);
     // 配列から指定した要素を削除
-    rows4.splice(0, splitNum);
+    rows4.splice(0, elementCount);
 }
 
 console.log(array4);

cfb82472b91cf45953a8f5ade68ef75d05fc8423
kgwny <kigawanaoya@gmail.com>
2021-06-18 23:25:01 +0900

    add snippet

---
+++ basic/array8.js
@@ -0,0 +1,6 @@
+var num = 6;
+var tmpArray = [""];
+for (var i = 1; i < num; i++) {
+    tmpArray.push("");
+}
+console.log(JSON.stringify(tmpArray));
---
+++ basic/array8.ts
@@ -0,0 +1,8 @@
+let num: number = 6;
+let tmpArray = [""];
+
+for (let i = 1; i < num; i++) {
+    tmpArray.push("");
+}
+
+console.log(JSON.stringify(tmpArray));

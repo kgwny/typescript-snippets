53de7a3cc47438c0f65c4a2b77d4168cd116fd5b
kgwny <kigawanaoya@gmail.com>
2021-06-20 23:43:35 +0900

    add snippet

---
+++ basic/array01.ts
@@ -0,0 +1,5 @@
+const arr = ["Red", "Green", "Blue"];
+console.log(arr);       // => ["Red", "Green", "Blue"]
+console.log(arr[0]);    // => "Red"
+console.log(arr[1]);    // => "Green"
+console.log(arr[2]);    // => "Blue"

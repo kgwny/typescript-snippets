c65426ce3b6dff126e97237ed00b9da3b21ae229
kgwny <kigawanaoya@gmail.com>
2021-09-17 23:58:40 +0900

    add snippet

---
+++ util/csv2tsv.js
@@ -0,0 +1,4 @@
+var src = "a,b,c";
+console.log("src = " + src);
+var result = src.replace(/,/g, "\t");
+console.log("result = " + result);
---
+++ util/csv2tsv.ts
@@ -0,0 +1,5 @@
+const src = "a,b,c";
+console.log("src = " + src);
+
+const result = src.replace(/,/g, "\t");
+console.log("result = " + result);
